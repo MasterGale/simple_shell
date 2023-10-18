ff5ba0344ca0cc7413932ef61fb85906e47f1872
MasterGale <ojogohkent@gmail.com>
2023-10-18 07:31:06 -0700

    string1.c edited

--- string1.c
+++ string1.c
@@ -9,16 +9,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int d = 0;
 
 	if (dest == src || src == 0)
 		return (dest);
-	while (src[i])
+	while (src[d])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[d] = src[d];
+		d++;
 	}
-	dest[i] = 0;
+	dest[d] = 0;
 	return (dest);
 }
 
@@ -53,14 +53,14 @@ char *_strdup(const char *str)
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int d = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[d] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(str[d]);
+		d++;
 	}
 }
 
@@ -73,15 +73,15 @@ void _puts(char *str)
  */
 int _putchar(char c)
 {
-	static int i;
+	static int d;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || d >= WRITE_BUF_SIZE)
 	{
-		write(1, buf, i);
-		i = 0;
+		write(1, buf, d);
+		d = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[d++] = c;
 	return (1);
 }
