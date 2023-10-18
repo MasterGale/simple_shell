5b0567873fc0eb13ff5c94f98b26212fd1f347a7
MasterGale <ojogohkent@gmail.com>
2023-10-18 07:07:25 -0700

    lists correction

--- lists.c
+++ lists.c
@@ -8,16 +8,16 @@
  *
  * Return: size of list
  */
-list_t *add_node(list_t **head, const char *str, int num)
+list_l *add_node(list_t **head, const char *str, int num)
 {
-	list_t *new_head;
+	list_l *new_head;
 
 	if (!head)
 		return (NULL);
-	new_head = malloc(sizeof(list_t));
+	new_head = malloc(sizeof(list_l));
 	if (!new_head)
 		return (NULL);
-	_memset((void *)new_head, 0, sizeof(list_t));
+	_memset((void *)new_head, 0, sizeof(list_l));
 	new_head->num = num;
 	if (str)
 	{
@@ -41,7 +41,7 @@ list_t *add_node(list_t **head, const char *str, int num)
  *
  * Return: size of list
  */
-list_t *add_node_end(list_t **head, const char *str, int num)
+list_t *add_node_end(list_l **head, const char *str, int num)
 {
 	list_t *new_node, *node;
 
@@ -49,10 +49,10 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 		return (NULL);
 
 	node = *head;
-	new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_l));
 	if (!new_node)
 		return (NULL);
-	_memset((void *)new_node, 0, sizeof(list_t));
+	_memset((void *)new_node, 0, sizeof(list_l));
 	new_node->num = num;
 	if (str)
 	{
@@ -80,9 +80,9 @@ list_t *add_node_end(list_t **head, const char *str, int num)
  *
  * Return: size of list
  */
-size_t print_list_str(const list_t *h)
+size_l print_list_str(const list_l *h)
 {
-	size_t i = 0;
+	size_l i = 0;
 
 	while (h)
 	{
@@ -103,7 +103,7 @@ size_t print_list_str(const list_t *h)
  */
 int delete_node_at_index(list_t **head, unsigned int index)
 {
-	list_t *node, *prev_node;
+	list_l *node, *prev_node;
 	unsigned int i = 0;
 
 	if (!head || !*head)
@@ -140,9 +140,9 @@ int delete_node_at_index(list_t **head, unsigned int index)
  *
  * Return: void
  */
-void free_list(list_t **head_ptr)
+void free_list(list_l **head_ptr)
 {
-	list_t *node, *next_node, *head;
+	list_l *node, *next_node, *head;
 
 	if (!head_ptr || !*head_ptr)
 		return;
@@ -157,3 +157,4 @@ void free_list(list_t **head_ptr)
 	}
 	*head_ptr = NULL;
 }
+
