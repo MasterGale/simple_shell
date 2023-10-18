d4f3fef0e0bf014c807f83360ac5bf66b38f8960
MasterGale <ojogohkent@gmail.com>
2023-10-18 07:51:06 -0700

    main.c edited

--- main.c
+++ main.c
@@ -2,43 +2,43 @@
 
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * @arc: arg count
+ * @arv: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+int main(int arc, char **arv)
 {
 	info_t info[] = { INFO_INIT };
-	int k = 2;
+	int gr = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+		: "=r" (gr)
+		: "r" (gr));
 
-	if (ac == 2)
+	if (arc == 2)
 	{
-		k = open(av[1], O_RDONLY);
-		if (k == -1)
+		gr = open(arv[1], O_RDONLY);
+		if (gr == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
+				_eputs(arv[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
+				_eputs(arv[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readk = k;
+		info->readfd = gr;
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, av);
+	hsh(info, arv);
 	return (EXIT_SUCCESS);
 }
